0f79bd8c6f6f27972120616d898de7fd1c0b290f
mohammed khaled <31120975+mkj98@users.noreply.github.com>
2022-12-01 19:06:14 +0300

    upload

--- index.js
+++
@@ -1,9 +0,0 @@
-function logme(){
-    let password = prompt("Please enter password", "");
-    
-    if (password == null || password == "" || password !="9899") {
-      alert("fuck you :)")
-    } else {
-        document.getElementById("log").classList.add('hide');
-    }
-}
